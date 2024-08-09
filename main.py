27f8fd9c15b5c5f84b680a4083f5b79b92dece2d
minhgia2 <108081426+minhgia2@users.noreply.github.com>
2024-08-09 20:58:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from keep_alive import keep_alive
 
 status = "online" #online/dnd/idle
 
-GUILD_ID = ADD_YOUR_SERVER_ID_HERE
-CHANNEL_ID = ADD_YOUR_CHANNEL_ID_HERE
+GUILD_ID = 1081611251462975528
+CHANNEL_ID = 1081611252033388699
 SELF_MUTE = True
 SELF_DEAF = False
 
