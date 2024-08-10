b132b1878e2d9926294fd74257fdf6e936db114c
minhgia2 <108081426+minhgia2@users.noreply.github.com>
2024-08-11 03:33:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,78 +8,46 @@ from keep_alive import keep_alive
 
 status = "idle" #online/dnd/idle
 
-custom_status = "youtube.com/@SealedSaucer" #If you don't need a custom status on your profile, just put "" instead of "youtube.com/@SealedSaucer"
+GUILD_ID = ADD_YOUR_SERVER_ID_HERE
+CHANNEL_ID = ADD_YOUR_CHANNEL_ID_HERE
+SELF_MUTE = True
+SELF_DEAF = False
 
 usertoken = os.getenv("TOKEN")
 if not usertoken:
-    print("[ERROR] Please add a token inside Secrets.")
-    sys.exit()
+  print("[ERROR] Please add a token inside Secrets.")
+  sys.exit()
 
 headers = {"Authorization": usertoken, "Content-Type": "application/json"}
 
-validate = requests.get("https://canary.discordapp.com/api/v9/users/@me", headers=headers)
+validate = requests.get('https://canary.discordapp.com/api/v9/users/@me', headers=headers)
 if validate.status_code != 200:
-    print("[ERROR] Your token might be invalid. Please check it again.")
-    sys.exit()
+  print("[ERROR] Your token might be invalid. Please check it again.")
+  sys.exit()
 
-userinfo = requests.get("https://canary.discordapp.com/api/v9/users/@me", headers=headers).json()
+userinfo = requests.get('https://canary.discordapp.com/api/v9/users/@me', headers=headers).json()
 username = userinfo["username"]
 discriminator = userinfo["discriminator"]
 userid = userinfo["id"]
 
-def onliner(token, status):
+def joiner(token, status):
     ws = websocket.WebSocket()
-    ws.connect("wss://gateway.discord.gg/?v=9&encoding=json")
+    ws.connect('wss://gateway.discord.gg/?v=9&encoding=json')
     start = json.loads(ws.recv())
-    heartbeat = start["d"]["heartbeat_interval"]
-    auth = {
-        "op": 2,
-        "d": {
-            "token": token,
-            "properties": {
-                "$os": "Windows 10",
-                "$browser": "Google Chrome",
-                "$device": "Windows",
-            },
-            "presence": {"status": status, "afk": False},
-        },
-        "s": None,
-        "t": None,
-    }
+    heartbeat = start['d']['heartbeat_interval']
+    auth = {"op": 2,"d": {"token": token,"properties": {"$os": "Windows 10","$browser": "Google Chrome","$device": "Windows"},"presence": {"status": status,"afk": False}},"s": None,"t": None}
+    vc = {"op": 4,"d": {"guild_id": GUILD_ID,"channel_id": CHANNEL_ID,"self_mute": SELF_MUTE,"self_deaf": SELF_DEAF}}
     ws.send(json.dumps(auth))
-    cstatus = {
-        "op": 3,
-        "d": {
-            "since": 0,
-            "activities": [
-                {
-                    "type": 4,
-                    "state": custom_status,
-                    "name": "Custom Status",
-                    "id": "custom",
-                    #Uncomment the below lines if you want an emoji in the status
-                    #"emoji": {
-                        #"name": "emoji name",
-                        #"id": "emoji id",
-                        #"animated": False,
-                    #},
-                }
-            ],
-            "status": status,
-            "afk": False,
-        },
-    }
-    ws.send(json.dumps(cstatus))
-    online = {"op": 1, "d": "None"}
+    ws.send(json.dumps(vc))
     time.sleep(heartbeat / 1000)
-    ws.send(json.dumps(online))
+    ws.send(json.dumps({"op": 1,"d": None}))
 
-def run_onliner():
-    os.system("clear")
-    print(f"Logged in as {username}#{discriminator} ({userid}).")
-    while True:
-        onliner(usertoken, status)
-        time.sleep(50)
+def run_joiner():
+  os.system("clear")
+  print(f"Logged in as {username}#{discriminator} ({userid}).")
+  while True:
+    joiner(usertoken, status)
+    time.sleep(30)
 
 keep_alive()
-run_onliner()
+run_joiner()
